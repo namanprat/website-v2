858b8860615417830a243bd891b43604f6db928a
namanprat <a.namanprat@gmail.com>
2023-06-12 22:44:08 +0530

    Yes

--- scripts/overlay-menu.js
+++ scripts/overlay-menu.js
@@ -1,32 +1,35 @@
-gsap.set("#overlay", {y:"-105%"});
-gsap.set("#socials a", {y:"100%"});
-gsap.set("#overlay-bg", {y:"-100%", opacity:0});
+function valueSet() {
+  gsap.set("#overlay", {y:"-105%"});
+  gsap.set(".menu-item a", {y:"100%" });
+  gsap.set(".social a", {y:"100%" , opacity:0});
+  gsap.set("#overlay-bg", {y:"-100%", opacity:0});
+}
 
-var tl = gsap.timeline({paused: true, reversed:true});
+function overlayAnimation() {
+  var tl = gsap.timeline({paused: true, reversed:true});
 
   tl
-  .to("#overlay, #overlay-bg", {
+  .to("#overlay", {
     y:"0",
     ease: "power4.inOut",
-    duration: 2
-  })
-
-  .to("#overlay-bg", {
-    opacity: 1,
-    duration: 1.4,
-    delay: -1,
-    ease: "power4.inOut",
-  })
-
-  .to("#socials a", {
-    y:"0",
-    delay: -2,
-    ease: "power4.inOut",
-    stagger: 0.1,
     duration: 1.75,
+  })
+
+  .to(".menu-item a , .social a", {
+    y: "0",
+    opacity: 1,
+    delay: -1,
+    duration: 1.75,
+    ease: "power4.inOut",
+    stagger: 0.15,
   })
 
   $(".menu-close, .menu-open").click (function() {
  tl.reversed() ? tl.play() : tl.reverse();
   })
+}
 
+gsap.registerPlugin(ScrollTrigger);
+gsap.config({ nullTargetWarn: false});
+valueSet();
+overlayAnimation();
