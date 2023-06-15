62bba0c3062f94886b640509969b50424e33ecef
namanprat <a.namanprat@gmail.com>
2023-06-15 17:36:42 +0530

    c

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,6 +1,6 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    tablet: { smooth: true },
-    smartphone: { smooth: true},
-});
+ const scroll = new LocomotiveScroll({
+     el: document.querySelector("[data-scroll-container]"),
+     smooth: true,
+     tablet: { smooth: true },
+     smartphone: { smooth: true},
+ });
