91d04b2340b2f1195ad63a0317d6d26fbde1ff73
namanprat <a.namanprat@gmail.com>
2023-07-12 13:37:00 +0530

    Minified

--- scripts/time.js
+++
@@ -1,6 +0,0 @@
-var getTime = function () {
-    document.getElementById("time").innerHTML = new Date().toLocaleString("en-IN",{timeZone:'Asia/Kolkata', timeStyle:'long', hourCycle:'h24'});
-}
-
-getTime ();
-setInterval(getTime,1000);
