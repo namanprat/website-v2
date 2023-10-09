bb9e3d1bd88dc191184611f16987a60092fced2b
namanprat <59867991+namanprat@users.noreply.github.com>
2023-10-09 19:31:07 +0530

    s

---
+++ scripts/loco.js
@@ -0,0 +1,20 @@
+(function () {
+    const locomotiveScroll = new LocomotiveScroll();
+})();
+
+const locomotiveScroll = new LocomotiveScroll({
+lenisOptions: {
+  wrapper: window,
+  content: document.documentElement,
+  lerp: 0.1,
+  duration: 1.1,
+  orientation: 'vertical',
+  gestureOrientation: 'vertical',
+  smoothWheel: true,
+  smoothTouch: false,
+  wheelMultiplier: 1,
+  touchMultiplier: 2,
+  normalizeWheel: true,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+},
+});
