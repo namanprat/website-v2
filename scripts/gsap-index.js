495497c50521b18735fda5c310295b189f897150
namanprat <59867991+namanprat@users.noreply.github.com>
2023-10-17 09:43:23 +0530

    s

--- scripts/gsap-index.js
+++ scripts/gsap-index.js
@@ -140,22 +140,27 @@ function xAxisScroll() {
 
 function textReveal() {
     window.addEventListener("DOMContentLoaded", (event) => {
-        let typeSplit = new SplitType("[text-split]", {
-            types: "words, chars",
-            tagName: "span"
-        });
+        // let typeSplit = new SplitType("[text-split]", {
+        //     types: "words",
+        //     tagName: "div"
+        // });
+        const splitted = new SplitType("[text-split]", {types: "words"});
+        this.splitWords = new SplitType(splitted.words, {types: "words",tagName: "spun"});
         $("[animate]").each(function(index) {
             let tl = gsap.timeline({
                 paused: true
             });
-            tl.from($(this).find(".char"), {
-                opacity: 0,
+            tl.from($(this).find(".word"), {
+                // opacity: 0,
                 yPercent: 100,
-                duration: 1.3,
+                duration: 2.5,
                 ease: "expo.inOut",
-                stagger: {
-                    amount: 0.3
-                }
+            });
+            tl.from($(this).find(".spon"), {
+                // opacity: 0,
+                yPercent: 100,
+                duration: 3,
+                ease: "expo.inOut",
             });
             ScrollTrigger.create({
                 trigger: $(this),
