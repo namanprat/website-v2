00b2d06051064f268f50ed5b00d4a5e4c232f0f7
namanprat <59867991+namanprat@users.noreply.github.com>
2023-10-28 17:47:34 +0530

    s

--- scripts/move.js
+++ scripts/move.js
@@ -1,52 +1,23 @@
-const section = document.querySelector('section')
+const title = document.querySelector('h1')
+const links = document.querySelectorAll('section a')
+const body = document.querySelector('body')
 
-const image = section.querySelector('div.image')
-const shadow = section.querySelector('div.shadow')
-const title = section.querySelector('h2')
-const titlePink = section.querySelector('div.image h2')
+links.forEach((link) => {
+  link.addEventListener('mouseenter', () => {
+    title.innerText = link.getAttribute('data-title')
 
-let aimX = 0
-let aimY = 0
-let imageX = 0
-let imageY = 0
-let shadowX = 0
-let shadowY = 0
-let titleX = 0
-let titleY = 0
-let backgroundX = 0
-let backgroundY = 0
+    body.classList.add('hovered')
+    link.classList.add('hovered')
 
-const move = (event) => {
-  aimX = (2 * event.clientX) / window.innerWidth - 1
-  aimY = (2 * event.clientY) / window.innerHeight - 1
-}
+    body.classList.add(link.getAttribute('data-color'))
+  })
 
-const animate = () => {
-  imageX += (aimX - imageX) * 0.1
-  imageY += (aimY - imageY) * 0.1
+  link.addEventListener('mouseleave', () => {
+    title.innerText = 'Talk Talk'
 
-  shadowX += (aimX - shadowX) * 0.05
-  shadowY += (aimY - shadowY) * 0.05
+    body.classList.remove('hovered')
+    link.classList.remove('hovered')
 
-  titleX += (aimX - titleX) * 0.2
-  titleY += (aimY - titleY) * 0.2
-
-  backgroundX += (aimX - backgroundX) * 0.2
-  backgroundY += (aimY - backgroundY) * 0.2
-
-  image.style.transform = `translate(${3 * imageX}rem, ${3 * imageY}rem)`
-  shadow.style.transform = `translate(${3 * shadowX}rem, ${3 * shadowY}rem)`
-  title.style.transform = `translate(${1 * titleX}rem, ${1 * titleY}rem)`
-
-  image.style.backgroundPosition = `${50 - 25 * backgroundX}% ${40 - 20 * backgroundY}%`
-
-  let titlePinkX = 1 * titleX - 3 * imageX
-  let titlePinkY = 1 * titleY - 3 * imageY
-
-  titlePink.style.transform = `translate(${titlePinkX}rem, ${titlePinkY}rem)`
-
-  requestAnimationFrame(animate)
-}
-
-animate()
-document.addEventListener('mousemove', move)
+    body.classList.remove(link.getAttribute('data-color'))
+  })
+})
